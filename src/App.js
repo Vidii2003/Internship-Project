047eabdcdf36672e12116b1340272f9b8e75b752
Vidii2003 <gsvidya2003@gmail.com>
2024-09-21 16:14:32 +0530

    Login Page by Vidya

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Components/Pages/Login/Login";
 import ResetPass from "./Components/Pages/Login/resetpass";
 import Forgotmemberid from "./Components/Pages/Login/forgetmemberid";
+import SetupWizard from "./Components/Pages/Register/setupwizard";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Login />} />
         <Route path="/ResetPassword" element={<ResetPass />} />
         <Route path="/Memberid" element={<Forgotmemberid/>}/>
+        <Route path="/Register" element={<SetupWizard />} />
       </Routes>
     </Router>
   );
